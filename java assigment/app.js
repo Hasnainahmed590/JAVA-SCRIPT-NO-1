862d001d20ee5b2a08b7933b8fd7896e8f86435a
Hasnainahmed590 <93081058+Hasnainahmed590@users.noreply.github.com>
2022-04-01 22:06:05 +0500

    Add files via upload

---
+++ java assigment/app.js
@@ -0,0 +1,619 @@
+/////////// CHAPTER 1 ///////////
+
+
+// Alert
+
+// question 1
+// answer
+// alert();
+
+
+// question 2
+// answer
+// alert("Error! Please enter a valid password");
+
+
+// question 3
+// answer
+// alert("Welcome to JS Land...Happy Coding!" );
+
+
+// question 4
+// answer
+// alert("Welcome to JS Land..." );
+
+
+// question 4
+// answer
+// alert("Happy Coding!" );
+
+
+// question 5
+// answer
+// alert("Hello... I can JS through my web browser’s console" );
+
+
+// question 7
+// answer
+// d.
+
+
+/////////// CHAPTER 2 ///////////
+
+
+// VARIABLES FOR STRINGS
+
+
+
+// question 1
+// answer
+// var username ;
+
+
+// question 2
+// answer
+// var myName = "rafay";
+
+
+// question 3
+// answer
+// var message = "Hello World";
+// alert(message);
+
+
+// question 4
+// answer
+// var age = 15 +  + "years old" 
+// alert(age)
+
+
+// question 5
+// answer
+// var message = <h1> PIZZA </h1> +  + <h2> PIZZ </h2> +  + <h3> PIZ </h3> +  + <h4> PI </h4> +  + <h5> P </h5> 
+// alert(message)
+
+// question 6
+// answer
+// var email = "My email address is (arafay990086@gmail.com)"
+// alert(email)
+
+
+// question 7
+// answer
+// var book = "I am trying to learn from the Book A smarter way to learn JavaScript"
+// alert(book)
+
+
+// question 8
+// answer
+// var message = "Yah! I can write HTML content through Javascript"
+//  document.write(message)
+
+
+/////////// CHAPTER 3 ///////////
+
+
+
+// VARIABLES FOR NUMBERS
+
+
+
+// question 1
+// answer
+// var age = "I am 15 year old"
+// alert(age)
+
+
+// question 2
+// answer
+// var webpage = "you have visited this 14 time"
+// alert(webpage)
+
+
+// question 3
+// answer
+// var birthYear = "My birth year is 2003 " + <br> + "Data type of my ddeclared variable is number"
+// document.write(birthYear)
+
+
+// question 4
+// answer
+// var massage = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store"
+// document.write(massage)
+
+
+
+/////////// CHAPTER 4 ///////////
+
+
+
+//        VARIABLE NAMES: LEGAL & ILLEGAL      //
+
+
+// question 1
+// answer
+// var message1 = ""
+// var message2 = ""
+// var message3 = ""
+// var totle = message1 + message2 + message3
+
+
+/////////// CHAPTER 5 ///////////
+
+
+
+//     MATH EXPRESSIONS      //
+
+
+// question 1
+// answer
+// var num1 = 3
+// var num2 = 5
+// var sum = num1 + num2
+// document.write(sum)
+
+
+// question 2
+// answer
+// var num1 = 3
+// var num2 = 5
+// var subtraction = num1 - num2
+// document.write(subtraction)
+
+
+// question 2
+// answer
+// var num1 = 3
+// var num2 = 5
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+
+// question 2
+// answer
+// var num1 = 3
+// var num2 = 5
+// var modulus = num1 \ num2
+// document.write(modulus)
+
+
+// question 4
+// answer
+// var num1 = 600
+// var num2 = 5
+// var totle = num1 + num2
+// document.write(totle)
+
+
+// question 5
+// answer
+// document.write(Table of 4)
+
+// var num1 = 4
+// var num2 = 1
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+// var num1 = 4
+// var num2 = 2
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+// var num1 = 4
+// var num2 = 3
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+// var num1 = 4
+// var num2 = 4
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+// var num1 = 4
+// var num2 = 5
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+// var num1 = 4
+// var num2 = 6
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+// var num1 = 4
+// var num2 = 7
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+// var num1 = 4
+// var num2 = 8
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+// var num1 = 4
+// var num2 = 9
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+// var num1 = 4
+// var num2 = 10
+// var multiplication = num1 * num2
+// document.write(multiplication)
+
+
+// question 7
+// answer
+// var priceitem1 = 650
+// var quantityitem1 = 3
+// var totleitem1 = priceitem1 * quantityitem1
+// var priceitem2 = 100
+// var quantityitem2 = 7
+// var totleitem2 = priceitem2 * quantityitem2
+// var totle = totleitem1 + totleitem2 
+// document.write(totle)
+
+
+// question 8
+// answer
+// var totalmarks = 980
+// var obtainedmarks = 804
+// var percentage = obtainedmarks * 100 / 980
+// document.write(percentage)
+
+
+
+// question 9
+// answer
+// 
+// var doller = 104.80
+// var sudia = 28
+// var total = doller + sudia
+
+
+// question 11
+// answer
+// var current year = 2022
+// var birth year = 2003
+// var age is = 18
+
+/////////// CHAPTER 6-9 ///////////
+
+
+
+
+///////// MATH EXPRESSIONS /////////
+
+
+// question 2
+// answer
+// var x = 1;
+
+// if (x === 1) {
+//   var x = 2;
+
+//   console.log(x);
+//   // expected output: 2
+// }
+
+// console.log(x);
+// // expected output: 2
+
+
+
+ ///////////// CHAPTER 9-11  /////////// 
+                                    
+// question 1
+// answer
+
+
+
+// cityName=prompt("Enter City Name");
+// if(cityName=="karachi"){
+//     console.log("Welcome to city of Lights");
+// }
+// else{
+//     console.log("Thanks for Choosing")
+// }
+
+
+// question 2
+// answer
+
+
+// gender=prompt("Enter Your Gender");
+// if(gender==="male"){
+//     console.log("Good Morning Sir!");
+// }
+// else if(gender==="female"){
+//     console.log("Good Morning Ma'am!");
+// }
+// else{
+//     console.log("Invalid Gender")
+// }
+
+
+// question 3
+//asnwer
+
+
+// signalColor=prompt("Enter Signal Color");
+// if(signalColor==="Red"){
+//     console.log("Must Stop!");
+// }
+// else if(signalColor==="Yellow"){
+//     console.log("Ready to move");
+// }
+// else if(signalColor==="Green"){
+//     console.log("Move now!");
+// }
+// else{
+//     console.log("Inappropriate Signal color");
+// }
+
+
+
+// question 4
+//answer
+
+
+// fuel=+prompt("Enter Remaining Fuel in Car");
+// if(fuel<0.25){
+//     console.log("Please Refill the Fuel in Your Car");
+// }
+// else{
+//     console.log("No Need to Refill the Fuel")
+// }
+
+
+// 5. Run this script, & check whether alert message would be
+// displayed or not. Record the outputs.
+
+// a. var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// Yes,alert message will display because this code works properly.
+
+// b.var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+
+// c. var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+
+
+
+// d. var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+
+
+
+
+// e. if (true){
+    // alert("True");
+    // }
+    // if (false){
+    // alert("False");
+    // }
+
+
+
+// f. if("car" < "cat"){
+    // alert("car is smaller than cat");
+    // }
+
+
+
+
+
+// question 6
+// answer
+
+// var math=+prompt("Enter Your Mathematics Marks");
+// var physics=+prompt("Enter Your Physics Marks");
+// var computer=+prompt("Enter Your Computer Marks");
+// var totalMarks=+prompt("Enter Your Total Marks");
+// console.log("Your Total Marks is:"+" "+totalMarks);
+// var marksObtained=math+physics+computer;
+// console.log("Your Obtained Marks is:"+" "+marksObtained);
+// var percentage=(marksObtained/totalMarks)*100;
+// console.log("Your percentage is:"+" "+percentage);
+
+// if(percentage>=80 && percentage<=100){
+//     console.log("Your Grade is: A-one");
+//     console.log("Remarks: Marvellous Work")
+// }
+// else if(percentage>=70 && percentage<80){
+//     console.log("Your Grade is: A");
+//     console.log("Remarks: Good Work")
+// }
+// else if(percentage>=60 && percentage<70){
+//     console.log("Your Grade is: B");
+//     console.log("Remarks: You need to improve")
+// }
+// else if(percentage>=50 && percentage<60){
+//     console.log("Fail");
+//     console.log("Remarks: Sorry! Better Luck Next Time");
+// }
+// else {
+//     console.log("Invalid Percentage!");
+// }
+
+
+
+// question 7
+// answer
+
+
+// secNum=2;
+// alert("Choose a Secret Number from 1 to 10")
+// user=prompt("Enter a Secret Number")
+// if(secNum==user){
+//     console.log("Bingo! Correct answer")
+// }
+// else if(user==1 || user==3 || user==4 || user==5 || user==6 || user==7 || user==8 || user==9 || user==10){
+//     console.log("Close enough to the correct answer")
+// }
+
+
+// question 8
+//answer
+
+// num=+prompt("Enter a Number");
+// if(num%3==0){
+//         console.log(num+" "+"is divisible by 3");
+//     }
+//     else{
+//         console.log(num+ " "+"is not divisible by 3")
+//     }
+
+    
+//question 9
+//answer
+
+
+// num=+prompt("Enter Number");
+// if(num%2==0){
+//     console.log(num+" "+"is Even number");
+// }
+// else{
+//     console.log(num+ " "+"is Odd Number")
+// }
+
+
+
+// question 10
+// aneswer
+
+
+// temp=+prompt("Enter Temperature");
+// if(temp>40){
+//     console.log("It is too hot outside.")
+// }
+// else if(temp>30){
+//     console.log("The Weather today is Normal.")
+// }
+// else if(temp>20){
+//     console.log("Today's Weather is cool.")
+// }
+// else if(temp>10){
+//     console.log("OMG! Today's weather is so Cool.")
+// }
+// else{
+//     console.log("Inappropriate Temperature")
+// }
+
+//  question 11
+// answer
+
+
+// var firstNumber=+prompt("Enter First Number");
+// var secondNumber=+prompt("Enter Second Number");
+// var operation=prompt("Enter Operation");
+
+// if(operation=="+"){
+//     add=firstNumber+secondNumber;
+//     console.log("Your Result is:"+ " "+add);
+// }
+// else if(operation=="-"){
+//     sub=firstNumber-secondNumber;
+//     console.log("Your Result is:"+ " "+sub);
+// }
+// else if(operation=="*"){
+//     mul=firstNumber*secondNumber;
+//     console.log("Your Result is:"+ " "+mul);
+// }
+// else if(operation=="/"){
+//     div=firstNumber/secondNumber;
+//     console.log("Your Result is:"+ " "+div);
+// }
+// else if(operation=="%"){
+//     rem=firstNumber%secondNumber;
+//     console.log("Your Result is:"+ " "+rem);
+// }
+// else{
+//     console.log("Invalid Number or Operation")
+// }
+
+
+
+/////////// CHAPTER 12-13  ///////////
+
+
+
+// question 6
+// answer
+// var greeting;
+//  var hour = new Date().getHours();
+
+// if (hour < 18) {
+//     greeting = "Good day";
+// else
+//     greeting = "Good evening";
+// }
+
+// document.getElementById("demo").innerHTML = greeting;
+
+
+// question 7
+// answer
+// function print24(str)
+// {
+//     // Get hours
+//     var h1 = Number(str[1] - '0');
+//     var h2 = Number(str[0] - '0');
+//     var hh = (h2 * 10 + h1 % 10);
+  
+//     // If time is in "AM"
+//     if (str[8] == 'A')
+//     {
+//         if (hh == 12)
+//         {
+//             document.write("00");
+//             for (var i = 2; i <= 7; i++)
+//                 document.write(str[i]);
+//         }
+//         else
+//         {
+//             for (var i = 0; i <= 7; i++)
+//                 document.write(str[i]);
+//         }
+//     }
+  
+//     // If time is in "PM"
+//     else
+//     {
+//         if (hh == 12)
+//         {
+//             document.write("12");
+//             for (var i = 2; i <= 7; i++)
+//                 document.write(str[i]);
+//         }
+//         else
+//         {
+//             hh = hh + 12;
+//             document.write(hh);
+//             for (var i = 2; i <= 7; i++)
+//                 document.write(str[i]);
+//         }
+//     }
+// }
